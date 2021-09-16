5778eddc3887367cf029c392d49f8e12fe7dce8d
Joachim Dalen <me@d3v.no>
2021-09-16 21:04:02 +0200

    Upload test files to blob

--- src/Integration.FunctionApp/Functions/MultipartHttpTrigger.cs
+++ src/Integration.FunctionApp/Functions/MultipartHttpTrigger.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using JoachimDalen.AzureFunctions.Extensions.Attributes;
 using JoachimDalen.AzureFunctions.Extensions.Models;
@@ -6,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Integration.FunctionApp.Functions
 {
@@ -21,8 +24,17 @@ namespace Integration.FunctionApp.Functions
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "test/multipart")]
             HttpRequest req, ILogger log,
             [MultipartRequest(ValidateData = true)]
-            MultipartRequestData<MultipartRequestBodyData> multipartRequest)
+            MultipartRequestData<MultipartRequestBodyData> multipartRequest,
+            [Blob("multipart-files", FileAccess.Write)]
+            CloudBlobContainer blobContainer)
         {
+            foreach (var requestFile in multipartRequest.Files)
+            {
+                var blob = blobContainer.GetBlockBlobReference(requestFile.FileName);
+                blob.Properties.ContentType = MediaTypeNames.Application.Octet;
+                await blob.UploadFromByteArrayAsync(requestFile.Content, 0, requestFile.Content.Length);
+            }
+
             var data = new
             {
                 Id = multipartRequest.Data.Id
--- src/JoachimDalen.AzureFunctions.Extensions/Models/MultipartRequestData.cs
+++ src/JoachimDalen.AzureFunctions.Extensions/Models/MultipartRequestData.cs
@@ -3,6 +3,6 @@ namespace JoachimDalen.AzureFunctions.Extensions.Models
     public class MultipartRequestData<T>
     {
         public T Data { get; set; }
-        public MultipartFile Files { get; set; }
+        public MultipartFile[] Files { get; set; }
     }
 }
