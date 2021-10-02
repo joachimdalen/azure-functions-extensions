49d3cd2ef0f0574f8fd6c87f880b3341843ff0ce
Joachim Dalen <me@d3v.no>
2021-10-02 08:48:04 +0200

    Use script to set version

--- ci/get-updated-package-version.ps1
+++ ci/get-updated-package-version.ps1
@@ -1,32 +1,46 @@
-param ([Switch] $IncreaseMajor, [Switch] $IncreaseMinor, [Switch] $IncreasePatch)
+param ([Switch] $IncreaseMajor, [Switch] $IncreaseMinor, [Switch] $IncreasePatch, [string] $SelectedVersion)
 
-if (!(Get-Module -ListAvailable -Name SomeModule)) {
-    Install-Module -Name SemVerPS -Force
-} 
-
-
-$endpointUri = "https://azuresearch-usnc.nuget.org/query?q=JoachimDalen.AzureFunctions.TestUtils&prerelease=true";
-$body = (Invoke-WebRequest -Uri $endpointUri).Content
-
-$data = ConvertFrom-Json -InputObject $body;
-$version = $data[0].data.versions[-1].version;
-$semVer = ConvertTo-SemVer -Version $version;
-
-$major = $semVer.Major;
-if($IncreaseMajor){
-    $major = $major + 1;
+if ($SelectedVersion -ne "not-set") {
+    Write-Host "Version set manually";
+    Write-Output "##vso[task.setvariable variable=UPDATED_PACKAGE_VERSION;isOutput=true]$SelectedVersion"
+}
+else {
+    if (!(Get-Module -ListAvailable -Name SemVerPS)) {
+        Write-Host "Installing SemVerPS";
+        Install-Module -Name SemVerPS -Force
+    } 
+    
+    
+    $endpointUri = "https://azuresearch-usnc.nuget.org/query?q=JoachimDalen.AzureFunctions.TestUtils&prerelease=true";
+    $body = (Invoke-WebRequest -Uri $endpointUri).Content
+    
+    $data = ConvertFrom-Json -InputObject $body;
+    $version = $data[0].data.versions[-1].version;
+    $semVer = ConvertTo-SemVer -Version $version;
+    
+    Write-Host "Found existing package version $semVer";
+    
+    $major = $semVer.Major;
+    if ($IncreaseMajor) {
+        Write-Host "Updating major version"
+        $major = $major + 1;
+    }
+    
+    $minor = $semVer.Minor;
+    if ($IncreaseMinor) {
+        Write-Host "Updating minor version"
+        $minor = $minor + 1;
+    }
+    
+    $patch = $semVer.Patch;
+    if ($IncreasePatch) {
+        Write-Host "Updating patch version"
+        $patch = $patch + 1;
+    }
+    
+    $newVersion = [Semver.SemVersion]::New($sem.Major, $sem.Minor, $sem.Patch + 1, $sem.Prerelease)
+    
+    Write-Host "Setting new version $newVersion"
+    Write-Output "##vso[task.setvariable variable=UPDATED_PACKAGE_VERSION;isOutput=true]$newVersion"
 }
 
-$minor = $semVer.Minor;
-if($IncreaseMinor){
-    $minor = $minor + 1;
-}
-
-$patch = $semVer.Patch;
-if($IncreasePatch){
-    $patch = $patch + 1;
-}
-
-
-$newVersion = [Semver.SemVersion]::New($sem.Major, $sem.Minor, $sem.Patch + 1, $sem.Prerelease)
-Write-Output "##vso[task.setvariable variable=UPDATED_PACKAGE_VERSION;isOutput=true]$newVersion"
