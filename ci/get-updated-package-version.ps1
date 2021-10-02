54e8fe78c139ab1eeb658ab46fa09657e086273d
Joachim Dalen <me@d3v.no>
2021-10-02 07:49:46 +0200

    ci: Add package version script

---
+++ ci/get-updated-package-version.ps1
@@ -0,0 +1,32 @@
+param ([Switch] $IncreaseMajor, [Switch] $IncreaseMinor, [Switch] $IncreasePatch)
+
+if (!(Get-Module -ListAvailable -Name SomeModule)) {
+    Install-Module -Name SemVerPS
+} 
+
+
+$endpointUri = "https://azuresearch-usnc.nuget.org/query?q=JoachimDalen.AzureFunctions.TestUtils&prerelease=true";
+$body = (Invoke-WebRequest -Uri $endpointUri).Content
+
+$data = ConvertFrom-Json -InputObject $body;
+$version = $data[0].data.versions[-1].version;
+$semVer = ConvertTo-SemVer -Version $version;
+
+$major = $semVer.Major;
+if($IncreaseMajor){
+    $major = $major + 1;
+}
+
+$minor = $semVer.Minor;
+if($IncreaseMinor){
+    $minor = $minor + 1;
+}
+
+$patch = $semVer.Patch;
+if($IncreasePatch){
+    $patch = $patch + 1;
+}
+
+
+$newVersion = [Semver.SemVersion]::New($sem.Major, $sem.Minor, $sem.Patch + 1, $sem.Prerelease)
+Write-Host "##vso[task.setvariable variable=UPDATED_PACKAGE_VERSION;isOutput=true]$newVersion"
